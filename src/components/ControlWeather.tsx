51cd703fcec3723fe4de3225181f4fd126854206
emjorome <emjorome@espol.edu.ec>
2024-11-27 09:49:19 -0500

    update

--- src/components/ControlWeather.tsx
+++ src/components/ControlWeather.tsx
@@ -1,3 +1,6 @@
+{/* Hooks */ }
+import { useState, useRef } from 'react';
+ 
  {/* Componentes MUI */}
 
  import Paper from '@mui/material/Paper';
@@ -6,19 +9,38 @@
  import InputLabel from '@mui/material/InputLabel';
  import MenuItem from '@mui/material/MenuItem';
  import FormControl from '@mui/material/FormControl';
- import Select from '@mui/material/Select';
+ import Select, { SelectChangeEvent } from '@mui/material/Select';
     
  export default function ControlWeather() {
 
-     {/* Arreglo de objetos */}
-     let items = [
-         {"name":"Precipitación", "description":"Cantidad de agua que cae sobre una superficie en un período específico."}, 
-         {"name": "Humedad", "description":"Cantidad de vapor de agua presente en el aire, generalmente expresada como un porcentaje."}, 
-         {"name":"Nubosidad", "description":"Grado de cobertura del cielo por nubes, afectando la visibilidad y la cantidad de luz solar recibida."}
-     ]
+    {/* Constante de referencia a un elemento HTML */ }
+    const descriptionRef = useRef<HTMLDivElement>(null);
 
-     {/* Arreglo de elementos JSX */}
-     let options = items.map( (item, key) => <MenuItem key={key} value={key}>{item["name"]}</MenuItem> )
+    {/* Variable de estado y función de actualización */}
+    let [selected, setSelected] = useState(-1)
+
+    {/* Arreglo de objetos */}
+    let items = [
+        {"name":"Precipitación", "description":"Cantidad de agua que cae sobre una superficie en un período específico."}, 
+        {"name": "Humedad", "description":"Cantidad de vapor de agua presente en el aire, generalmente expresada como un porcentaje."}, 
+        {"name":"Nubosidad", "description":"Grado de cobertura del cielo por nubes, afectando la visibilidad y la cantidad de luz solar recibida."}
+    ]
+
+    {/* Arreglo de elementos JSX */}
+    let options = items.map( (item, key) => <MenuItem key={key} value={key}>{item["name"]}</MenuItem> )
+
+    {/* Manejador de eventos */}
+    const handleChange = (event: SelectChangeEvent) => {
+        
+        let idx = parseInt(event.target.value)
+        //alert( idx );
+        setSelected( idx );
+
+        {/* Modificación de la referencia descriptionRef */}
+        if (descriptionRef.current !== null) {
+            descriptionRef.current.innerHTML = (idx >= 0) ? items[idx]["description"] : ""
+        }
+    };
         
      {/* JSX */}
      return (
@@ -43,6 +65,7 @@
                          id="simple-select"
                          label="Variables"
                          defaultValue='-1'
+                         onChange={handleChange}
                      >
                          <MenuItem key="-1" value="-1" disabled>Seleccione una variable</MenuItem>
 
@@ -52,6 +75,14 @@
                  </FormControl>
 
              </Box>
+
+             {/* Use la variable de estado para renderizar del item seleccionado */}
+             {/*<Typography mt={2} component="p" color="text.secondary">
+             {
+                 (selected >= 0)?items[selected]["description"]:""
+             }
+             </Typography>*/}
+             <Typography ref={descriptionRef} mt={2} component="p" color="text.secondary" />
 
 
          </Paper>
